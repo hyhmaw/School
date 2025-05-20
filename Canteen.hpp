787ba5046552f10eb62611894c569c033e6d070f
user <05ukv10@gmail.com>
2025-05-20 20:27:33 +0300

    added school canteen

---
+++ Canteen.hpp
@@ -0,0 +1,15 @@
+#include <string>
+
+class Canteen{
+private:
+    string cooker;
+    string  schedule;
+public:
+    void addCooker(string cooker){
+        cooker = cooker;
+    }
+    void addSchedule(string s){
+        shedule = s;
+    }
+
+}
