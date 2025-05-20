e1e24dbf65ccec77984d2f46cffa474191098780
user <05ukv10@gmail.com>
2025-05-20 19:28:16 +0300

    Main

--- main.cpp
+++ main.cpp
@@ -19,5 +19,38 @@ using namespace std;
 
 int main {
     setlocale(0, ".1251"); 
+    shared_ptr<Student> s1 = make_shared<Student>("Ksenia", 18, "Kn-1.01", 85);
+    shared_ptr<Student> s2 = make_shared<Student>("Nastya", 19, "Kn-1.02", 95);
+    shared_ptr<Student> s3 = make_shared<Student>("Bob", 17, "Kn-1.01", 70);
+    shared_ptr<Teacher> t1 = make_shared<Teacher>("Bykata", 60, "C++", "Programming");
+    shared_ptr<Teacher> t2 = make_shared<Teacher>("Volkova", 45, "Math", "Higher math");
+    shared_ptr<Administrator> a1 = make_shared<Administrator>("Kaptyr Vadim", 50, "Dean", "Education");
+
+    University u;
+    u.students.addPerson(s1);
+    u.students.addPerson(s2);
+    u.students.addPerson(s3);
+    u.teachers.addPerson(t1);
+    u.teachers.addPerson(t2);
+    u.admins.addPerson(a1);
+
+    cout << "   University Registry:   \n";
+    u.students.listPeople();
+    u.teachers.listPeople();
+    u.admins.listPeople();
+
+    Course c1("Higher math", t2);
+    c1.addStudent(s1);
+    c1.addStudent(s3);
+
+    Course c2("Programming", t1);
+    c2.addStudent(s2);
+
+    cout << "\n  Math Course information:  \n";
+    c1.showCourseInfo();
+
+    cout << "\n  C++ Course information:  \n";
+    c2.showCourseInfo();
+
     return 0;
 }
